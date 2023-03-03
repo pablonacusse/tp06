8194415c8309adc7644fc42c9afe0bf387a13d4b
Pablo Marcelo Nacusse <pablo.nacusse@gmail.com>
2023-03-03 09:04:13 -0300

    Trabajo Práctivo Nro 6 - Pablo Nacusse

---
+++ build/obj/src/clock.d
@@ -0,0 +1,3 @@
+build/obj/src/clock.o: \
+ /home/pablo/eiti/2022/embebidos/tp/tp06/src/clock.c \
+ /home/pablo/eiti/2022/embebidos/tp/tp06/inc/clock.h
---
+++ build/obj/src/pantalla.d
@@ -0,0 +1,3 @@
+build/obj/src/pantalla.o: \
+ /home/pablo/eiti/2022/embebidos/tp/tp06/src/pantalla.c \
+ /home/pablo/eiti/2022/embebidos/tp/tp06/inc/pantalla.h
